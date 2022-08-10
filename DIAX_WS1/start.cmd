bb124f737bc0e2be71f1d8fa9d0860615d255fae
Gaetan <32365616+gvillant@users.noreply.github.com>
2022-08-10 17:07:41 +0200

    Update start.cmd

--- DIAX_WS1/start.cmd
+++ DIAX_WS1/start.cmd
@@ -8,12 +8,12 @@ If exist %USBDrive%Dell_Tools\ImageAssistPE.exe (
 xcopy %USBDrive%Dell_Tools x:\windows\system32\ClassicPeApplication /Y /S >NUL
 echo copied dependent files successfully)
 
+start x:\windows\system32\LogZipper\LogZipper.exe
+start x:\windows\system32\ClassicPeApplication\ImageAssistPE.exe
+
 If exist %USBDrive%Dell_Tools\VMware\VMwareWS1ProvisioningTool.msi (
 md O:\windows\Panther\Unattend >NUL
 xcopy %USBDrive%Dell_Tools\VMware\ O:\windows\Panther\Unattend /Y /S >NUL
 echo copied VMware files successfully)
-
-start x:\windows\system32\LogZipper\LogZipper.exe
-start x:\windows\system32\ClassicPeApplication\ImageAssistPE.exe
 
 :eof
