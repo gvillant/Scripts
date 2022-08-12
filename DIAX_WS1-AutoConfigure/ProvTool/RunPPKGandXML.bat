833b512a658947075a8e3a8f86e6db6ae922f644
Gaetan <32365616+gvillant@users.noreply.github.com>
2022-08-12 15:47:36 +0200

    Add files via upload

---
+++ DIAX_WS1-AutoConfigure/ProvTool/RunPPKGandXML.bat
@@ -0,0 +1,2 @@
+cd %~dp0
+msiexec /a "VMwareWS1ProvisioningTool.msi" /qb TARGETDIR="C:\Temp\VMwareWS1ProvisioningTool" && C:\Temp\VMwareWS1ProvisioningTool\VMwareWS1ProvisioningTool.exe -a full -p .\ProvisioningPackage.ppkg -u .\unattend.xml -s --gui
---
+++ DIAX_WS1-AutoConfigure/after_restore.cmd
@@ -0,0 +1,5 @@
+xcopy "U:\Dell_Tools\AuditMode\unattend.xml" "O:\Windows\Panther\" /cherky
+md "O:\Windows\Panther\WS1"
+xcopy "U:\Dell_Tools\ProvTool\*.*" "O:\Windows\Panther\WS1" /cherky
+
+
