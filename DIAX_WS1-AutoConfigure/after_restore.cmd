500df7dff0ca9674666db4af47a672ecadbbb8c4
Gaetan <32365616+gvillant@users.noreply.github.com>
2022-08-26 20:13:06 +0200

    Update after_restore.cmd

--- DIAX_WS1-AutoConfigure/after_restore.cmd
+++ DIAX_WS1-AutoConfigure/after_restore.cmd
@@ -1,5 +1,5 @@
 xcopy "U:\Dell_Tools\AuditMode\unattend.xml" "O:\Windows\Panther\" /cherky
 md "O:\Windows\Panther\WS1"
-xcopy "U:\Dell_Tools\ProvTool\*.*" "O:\Windows\Panther\WS1" /cherky
+xcopy "V:\Dell_Tools\ProvTool\*.*" "O:\Windows\Panther\WS1" /cherky
 
 
