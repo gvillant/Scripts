f4f90dde75b3cb921ef6d2b285e36a9f4e9cdccb
Gaetan <gaetan.villant@gmail.com>
2022-08-31 10:31:55 +0200

    New version v_220831a

--- DIAX_WS1/RunPPKGandXML.bat
+++
@@ -1,2 +0,0 @@
-cd %~dp0
-msiexec /a "VMwareWS1ProvisioningTool.msi" /qb TARGETDIR="C:\Temp" && C:\Temp\VMwareWS1ProvisioningTool\VMwareWS1ProvisioningTool.exe -a full -p .\Dropship-Package.ppkg -u .\Dropship-unattend.xml -s --gui
